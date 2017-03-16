ba87d188bfb205b337227df8a3021bc5b0abe8dc
Alexander Jasper <alja@netcompany.com>
2017-03-16 20:48:27 +0100

    Added logout page.

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+	include('conn.php');
+	session_unset();
+?>
+<html>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <title>Logget ud</title>
+  </head>
+<body>
+  Du er nu logget ud.
+  <p>
+    <a href="index.php">
+      <button style="height:50px;width:200px">Gå til startside</button>
+    </a>
+  </p>
+</body>
+</html>
