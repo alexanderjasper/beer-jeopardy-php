d5404cfb7587141add3a1e9c6eef530779d49e95
Alexander Jasper <alja@netcompany.com>
2017-03-16 20:49:44 +0100

    Automatically go to control panel if already logged in.

---
+++ brugeroprettet.output.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <title>Bruger oprettet</title>
+  </head>
+<body>
+  Brugeren er oprettet.
+  <p>
+    <form action="kontrolpanel.php" method="post">
+      <button style="height:50px;width:200px">Gå til kontrolpanel</button>
+      <input type="hidden" name="brugernavn" value="<?php echo $name?>"
+    </a>
+  </p>
+</body>
+</html>
