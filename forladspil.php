f1ac64ce8719060bbc38302441fbd3b1a5675d13
Alexander Jasper <alja@netcompany.com>
2017-03-25 17:33:04 +0100

    Tur is now passed on if the active player leaves the game.

--- forladspil.php
+++ forladspil.php
@@ -6,7 +6,7 @@ $uid = $_SESSION['userid'];
 $sid = $_SESSION['spilid'];
 
 $sql = mysqli_query($link,
-"SELECT spilkategori.spilkategoriid
+"SELECT spilkategori.spilkategoriid, deltager.tur
 FROM deltager
 JOIN spilkategori ON deltager.deltagerid=spilkategori.deltagerid
 WHERE deltager.brugerid='$uid'
@@ -16,6 +16,14 @@ $spilkatid = $row['spilkategoriid'];
 
 mysqli_query($link, "DELETE FROM deltager WHERE spilid='$sid' AND brugerid='$uid'");
 mysqli_query($link, "DELETE FROM spilkategori WHERE spilkategoriid='$spilkatid'");
+
+if($row['tur'] == 1 or $row['tur'] == 2)
+{
+  $sql = mysqli_query($link, "SELECT deltagerid FROM deltager WHERE spilid='$sid'");
+  $row = mysqli_fetch_assoc($sql);
+  $firstdeltager = $row['deltagerid'];
+  mysqli_query($link, "UPDATE deltager SET tur='1' WHERE deltagerid='$firstdeltager'");
+}
 
 
 
